d5e30ee2f694cc64077a47bfc24377572c1532b9
MTTA96 <mtta200396@gmail.com>
2017-10-19 02:14:04 +0700

    Fix bug db khóa học

--- Code/ELearning/ELearning/Migrations/201710181021252_TaoTableKhoaHoc.cs
+++ Code/ELearning/ELearning/Migrations/201710181021252_TaoTableKhoaHoc.cs
@@ -27,16 +27,16 @@ namespace ELearning.Migrations
                         NgayDang = c.DateTime(nullable: false),
                         SoBuoi = c.Int(nullable: false),
                         HocPhi = c.Double(nullable: false),
-                        Buoi_Id = c.Byte(nullable: false),
-                        Thu_Id = c.Byte(nullable: false),
+                        BuoiId = c.Byte(nullable: false),
+                        ThuId = c.Byte(nullable: false),
                     })
                 .PrimaryKey(t => t.Id)
-                .ForeignKey("dbo.Buois", t => t.Buoi_Id, cascadeDelete: true)
+                .ForeignKey("dbo.Buois", t => t.BuoiId, cascadeDelete: true)
                 .ForeignKey("dbo.AspNetUsers", t => t.ThanhVienId, cascadeDelete: true)
-                .ForeignKey("dbo.Thus", t => t.Thu_Id, cascadeDelete: true)
+                .ForeignKey("dbo.Thus", t => t.ThuId, cascadeDelete: true)
                 .Index(t => t.ThanhVienId)
-                .Index(t => t.Buoi_Id)
-                .Index(t => t.Thu_Id);
+                .Index(t => t.BuoiId)
+                .Index(t => t.ThuId);
             
             CreateTable(
                 "dbo.Thus",
@@ -51,11 +51,11 @@ namespace ELearning.Migrations
         
         public override void Down()
         {
-            DropForeignKey("dbo.KhoaHocs", "Thu_Id", "dbo.Thus");
+            DropForeignKey("dbo.KhoaHocs", "ThuId", "dbo.Thus");
             DropForeignKey("dbo.KhoaHocs", "ThanhVienId", "dbo.AspNetUsers");
-            DropForeignKey("dbo.KhoaHocs", "Buoi_Id", "dbo.Buois");
-            DropIndex("dbo.KhoaHocs", new[] { "Thu_Id" });
-            DropIndex("dbo.KhoaHocs", new[] { "Buoi_Id" });
+            DropForeignKey("dbo.KhoaHocs", "BuoiId", "dbo.Buois");
+            DropIndex("dbo.KhoaHocs", new[] { "ThuId" });
+            DropIndex("dbo.KhoaHocs", new[] { "BuoiId" });
             DropIndex("dbo.KhoaHocs", new[] { "ThanhVienId" });
             DropTable("dbo.Thus");
             DropTable("dbo.KhoaHocs");
